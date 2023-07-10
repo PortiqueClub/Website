845b80dd8f906f819876d1548df9aca049838b26
malolebrin <malolebrin@gmail.com>
2023-07-10 11:09:18 +0200

    🩹 FIx lint

--- app.vue
+++ app.vue
@@ -2,10 +2,14 @@
 <div>
   <NuxtWelcome />
   <select v-model="locale">
-        <option value="en">en</option>
-        <option value="fr">fr</option>
-      </select>
-      <p>{{ $t('welcome') }}</p>
+    <option value="en">
+      en
+    </option>
+    <option value="fr">
+      fr
+    </option>
+  </select>
+  <p>{{ $t('welcome') }}</p>
 </div>
 </template>
 

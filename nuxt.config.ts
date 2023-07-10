b89797ba9f11f1ea13c992eaf72ba9e67b33967c
malolebrin <malolebrin@gmail.com>
2023-07-10 10:05:22 +0200

    🎉 Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: import.meta.dev },
+
+  modules: [
+    // '@nuxtjs/tailwindcss',
+    // '@intlify/nuxt3',
+  ],
+
+  // target: 'static',
+
+  typescript: {
+    strict: true,
+  },
+
+  ssr: false,
+
+  head: {
+    title: 'Le Portique club',
+    meta: [
+      { charset: 'utf-8' },
+      { title: 'Unplugin' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Le Portique Club , depuis 1932, anime des activités et propose des cours de natation dans une piscine chauffée sur la plage pour les enfants pendant la période estivale. Il accueille vos enfants de 3 à 13 ans, du Dimanche au Vendredi (fermeture le Samedi) de 10h à 13h et de 15h30 à 18h30, de début Juillet à fin Août. L’inscription sur la plage est simple et rapide : à la journée, à la demi-journée ou à la semaine. Nous acceptons les chèques vacances. Le club se situe sur la Plage centrale de la Tranche-sur-Mer, à proximité du centre-ville.' },
+      { hid: 'keywords', name: 'keywords', content: 'animation, enfants, piscine, activités, cours de natation, tranche sur mer' },
+    ],
+  },
+})
